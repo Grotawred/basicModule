50a54e333eb45e8ccbd0ba5d29c038c0f494788d
Grotawred <grishavlasko200@gmail.com>
2023-10-31 09:25:05 +0200

    bug fix

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from setuptools import setup
 
-with open("README.md", "r",encoding="utf-8") as file:
-    long_description = file.read
-
-
 setup(
     name='linux-comands',
     version=1.0,
-    description="Basic linux comands",
-    long_description=long_description,
     author="Grisha Vlasko",
     author_email="grishavlasko200@gmail.com",
     long_description_content_type="text/markdown",
